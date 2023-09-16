c9fce797f927081afd2a79689283d44a55b2c441
twinklelittlestars <cmty_0916@qq.com>
2023-09-17 02:18:04 +0800

    lab0_code

---
+++ Fibonacci.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,i,t,n;
+
+    a=0;
+    b=1;
+    i=1;
+    cin>>n;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    while(i<n)
+    {
+        t=b;
+        b=b+a;
+        cout<<b<<endl;
+        a=t;
+        i=i+1;
+    }
+    return 0;
+}
+
